e515e577fa5c780e86653cb07ad5be31b8ef9550
sandersonsa <costadesa@gmail.com>
2020-11-05 15:28:02 -0300

    atualizar-exemplo

--- src/main/java/sa/sanderson/redhat/basa/democamelspringswagger/routes/PrincipalRoute.java
+++ src/main/java/sa/sanderson/redhat/basa/democamelspringswagger/routes/PrincipalRoute.java
@@ -9,17 +9,14 @@ public class PrincipalRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        
-        rest("/rest")
-            .tag("Demo API 1")
-            .consumes("application/text").produces("application/text")
-            
-            .get("ping")
-                .route()
-                    .routeId("ping")
-                    .transform(constant("PONG\n"))
-                .endRest();
-        ;
+        rest("/say")
+            .get("/hello").to("direct:hello")
+            .get("/bye").consumes("application/json").to("direct:bye");
+
+        from("direct:hello")
+            .transform().constant("Hello World");
+        from("direct:bye")
+            .transform().constant("Bye World");
     }
     
 }
--- src/main/java/sa/sanderson/redhat/basa/democamelspringswagger/routes/PrincipalRoute2.java
+++ src/main/java/sa/sanderson/redhat/basa/democamelspringswagger/routes/PrincipalRoute2.java
@@ -9,17 +9,44 @@ public class PrincipalRoute2 extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        
-        rest("/rest2")
-            .tag("Demo API 2")
-            .consumes("application/text").produces("application/text")
-            
-            .get("ping")
-                .route()
-                    .routeId("ping")
-                    .transform(constant("PONG\n"))
-                .endRest();
-        ;
+        rest("/say2")
+            .get("/hello2").to("direct:hello")
+            .get("/bye2").consumes("application/json").to("direct:bye");
+
+        from("direct:hello2")
+            .transform().constant("Hello World");
+        from("direct:bye2")
+            .transform().constant("Bye World");
     }
+
+    // @Override
+    // public void configure() throws Exception {
+    //     restConfiguration()
+    //     .component("servlet")
+    //     .bindingMode(RestBindingMode.json);
+
+    //     rest().get("/hello")
+    //     .to("direct:hello");
+    
+    //     from("direct:hello")
+    //     .log(LoggingLevel.INFO, "Hello World")
+    //     .transform().simple("Hello World");
+    // }
+
+    // https://github.com/Java-Techie-jt/spring-camel-rest-dsl //
+
+    // @Override
+	// public void configure() throws Exception {
+	// 	restConfiguration().component("servlet").port(9090).host("localhost").bindingMode(RestBindingMode.json);
+
+	// 	rest().get("/hello-world").produces(MediaType.APPLICATION_JSON_VALUE).route()
+	// 			.setBody(constant("Welcome to java techie")).endRest();
+
+	// 	rest().get("/getOrders").produces(MediaType.APPLICATION_JSON_VALUE).route().setBody(() -> service.getOrders())
+	// 			.endRest();
+
+	// 	rest().post("/addOrder").consumes(MediaType.APPLICATION_JSON_VALUE).type(Order.class).outType(Order.class)
+	// 			.route().process(processor).endRest();
+	// }
     
 }
